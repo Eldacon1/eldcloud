20d15f55ab7da6ddfbd1d2a7a960ae688dd3722e
jsaak <jsaak@masina.(none)>
2011-08-30 17:08:57 +0200

    added a rails empty skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eldcloud::Application.config.secret_token = 'e5b0a5beb5222a9c055ff088689b6126284f10d2b0a0a2b9aa5d751761c23609e16f473db92345dfeb2bddfe4d4f4d43df5fab99d7c997d88971ef267f746bfb'
